49b36ebb99b1535502c6d3813d5d189c6ed4e640
Likhit Gudisay <l_gudisay@ucsb.edu>
2024-05-27 14:30:52 -0700

    add stuff

--- src/UserManagement.sol
+++ src/UserManagement.sol
@@ -5,26 +5,42 @@ contract UserManagement {
 
     // struct to contain item information
     struct Item {
-        string name;
-        uint256 value;
-        address owner;
+        uint256 id;          //item index value
+        string name;         //item name
+        uint256 lowPrice;    //lowest expected price
+        uint256 highPrice;   //high of price range
+        string desc;         //description of item
+        string category;    // type of item (tech, fashion, etc)
+        address owner;       //addr of owner
+        uint256 currBid;     //value of currBid price
+        address highestBidder;  //addr of curr highest bid
+        uint256 timePosted;  //block.timestamp of when item is listed
     }
 
     // struct to contain user information
     struct User {
-        uint8 role;         // role for user 0: unregistered, 1: registered, 2: admin (possibly unneeded)
-        uint256 balance;    // balance for user
-        mapping(uint256 => Item) items; // array of items that the user owns, each item in the array is an Item struct
-        uint256 numItems;   // number of items the user currently owns
+        uint8 role = 0;         // role for user 0: unregistered, 1: registered, 2: admin (possibly unneeded)
+        uint256 balance = 0;    // balance for user
+        Item[] items = []; // array of items that the user owns, each item in the array is an Item struct
+        uint256 numCart = 0;   // number of items in cart
+        uint256 numSale = 0;   //number of items in market
+        uint256 currBids = 0;   //number of items w user as highest bidder
     }
 
     address private admin; // address of admin
 
-    //maps an address to a User
-    mapping(address => User) public users;
+    uint256 private itemIndex = 1;  //global counter for item idx
 
-    // array of all items in Market
+    uint256 public constant saleTime = 2 weeks;   //how long items can remain in store for
+
+
+    //maps an address to a User
+    mapping(address => User) private users;
+
+    // array of all items in Market, will work as a heap with items sorted by earliest post date
     Item[] private market;
+
+    mapping(uint256 => uint256) private spotInStore;  // keeps track of location in market for each item
 
     constructor() {
         // set user as admin
@@ -32,9 +48,16 @@ contract UserManagement {
         
         // create dummy item for default item in market to connect to
         Item memory item = Item({
-            name: "dummy item",
-            value: 0,
-            owner: address(0)
+            id: 0,
+            name: "empty item",
+            lowPrice: 0,
+            highPrice: 0,
+            desc: "empty",
+            itemType: 0,
+            owner: address(0),
+            currBid: 0,
+            highestBidder: address(0),
+            timePosted: block.timestamp
         });
         market.push(item);
     }
@@ -58,8 +81,25 @@ contract UserManagement {
         }
     }
 
+    // removes balance to user
+    function removeBalance(uint256 amount) public returns (bool) {
+        if (users[msg.sender].role == 1 || users[msg.sender].role == 2) {
+            if(users[msg.sender].balance >= amount) {
+                users[msg.sender].balance -= amount;
+                return true;
+            }
+            else{
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+
+
     // adds item to user
-    function addItemToUser(address userAddress, string memory itemName, uint256 itemValue) public {
+    function addItem(string itemName, uint256 lowEnd, uint256 highEnd, string itemDesc, string itemType) public {
         // Create a new Item
         Item memory newItem = Item({name: itemName, value: itemValue, owner: userAddress});
 
